5d0afab1b492657044cf844e87e21a0b64d2e0d4
Emerson Lima <emersonb18@hotmail.com>
2020-10-03 05:59:08 -0300

    teste vs code

--- src/main/java/com/spring/codeblog/utils/DummyDate.java
+++ src/main/java/com/spring/codeblog/utils/DummyDate.java
@@ -24,17 +24,15 @@ public class DummyDate {
         post1.setAutor("Bruno Alexandre");
         post1.setData(LocalDate.now());
         post1.setTitulo("Docker");
-        post1.setTexto("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+        post1.setTexto("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including.");
 
         Post post2 = new Post();
         post2.setAutor("Michelli Brito");
         post2.setData(LocalDate.now());
         post2.setTitulo("API REST");
         post2.setTexto("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
-
         postList.add(post1);
         postList.add(post2);
-
         for(Post post: postList){
             Post postSaved = postService.save(post);
             System.out.println(postSaved.getId());
